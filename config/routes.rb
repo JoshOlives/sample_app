d832e05cd9f9146b5d3f991c0bf5dea562035437
Onions <joshuaolivares@utexas.edu>
2019-01-23 04:50:12 +0000

    showing to vanessa

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   
   get 'static_pages/contact'
+  
+  get 'static_pages/log_in'
 
       # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
