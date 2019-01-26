dfe3809a84cf0d59d1de564effd1fa381f827aed
Onions <joshuaolivares@utexas.edu>
2019-01-26 22:19:07 +0000

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new #why do i need this
   end
   def show
     @user = User.find(params[:id])
+    #debugger
   end
+  
+  def create
+    @user = User.new(user_params) #what?
+    if @user.save
+      flash[:success] = "Welcome :P"
+      redirect_to @user #shorthand for user_url(@user) diff between url and path??
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
